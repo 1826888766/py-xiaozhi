0b7efe828ee85a1a5120c7b60d494b11ab2f8da9
马良 <1826888766@qq.com>
2025-12-15 17:05:40 +0800

    tts合成

--- src/mcp/tools/yours/tts_service.py
+++ src/mcp/tools/yours/tts_service.py
@@ -54,14 +54,14 @@ class TTSService:
     def send_tts_state_changed(self, state):
         if state == "idle":
             ## 恢复监听\
-            self.app.spawn(self.app.start_listening_manual())
+            self.app.spawn(self.app.start_listening_manual(),"main:listening")
         if state == "playing":
             ## 暂停监听
             if self.app.is_speaking():
-                self.app.spawn(self.app.abort_speaking("tts playing"))
+                self.app.spawn(self.app.abort_speaking("tts playing"),"main:abort")
             if self.app.is_listening():
-                self.app.spawn(self.app.stop_listening_manual())
-        self.app.spawn(self.app.plugins.notify_tts_state_changed(state))
+                self.app.spawn(self.app.stop_listening_manual(),"main:stop")
+        self.app.spawn(self.app.plugins.notify_tts_state_changed(state),"plugins:tts_state")
 
     def _play_next(self):
 
